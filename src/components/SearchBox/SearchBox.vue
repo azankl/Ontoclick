64e93c52e94e6588b8c455ccb66f942b6a356b7e
Anthony <anthony.a.xu@hotmail.com>
2020-07-22 00:08:24 +1000

    added other API to search from

--- src/components/SearchBox/SearchBox.vue
+++ src/components/SearchBox/SearchBox.vue
@@ -8,7 +8,7 @@
   </div>
   <v-server-table :url="url" :columns="columns" :options="options">
     <div slot='conceptRec' class='form-group'>
-      <treeselect :multiple="false" :clearable="false" :close-on-select="true" :flat="true" :select="getCR()" :options="conceptrecogniserOptions" v-model="conceptrecogniserValue" placeholder="Select Concept Recognizer" name="conceptRecogniser" />
+      <treeselect :multiple="false" :clearable="false" :close-on-select="true" :flat="true" :options="conceptrecogniserOptions" v-model="conceptrecogniserValue" placeholder="Select Concept Recognizer" name="conceptRecogniser" />
     </div>
     <div slot='ontologiesFilter' class='form-group'>
         <treeselect :multiple="true" :clearable="false" :close-on-select="true" :flat="true" :options="ontologyOptions" style="z-index:6;" placeholder="Filter by Ontology" v-model="ontologyValue" />
@@ -17,23 +17,19 @@
         <div class='text-wrap' v-if="props.row.definition"><b>Definition: </b>{{props.row.definition[0]}}</div>
         <div class='text-wrap' v-if="props.row.synonym"><b>Synonyms: </b>{{ typeof props.row.synonym === 'string' ? props.row.synonym : props.row.synonym.join(', ') }}</div>
       </template>
-    <template slot="notation" scope="props" v-if="props.row.notation || props.row.id">
+    <template slot="notation" scope="props" v-if="props.row.notation">
         <span :id='"notation"+props.index' v-if="props.row.notation">{{props.row.notation}}</span>
-        <span :id='"notation"+props.index' v-if="props.row.id">{{props.row.id}}</span>
         <a class="hover-action fa fa-copy" @click='copyContent("notation"+props.index)'></a>
       </template>
-    <template slot="prefLabel" scope="props" v-if="props.row.prefLabel || props.row.name">
+    <template slot="prefLabel" scope="props" v-if="props.row.prefLabel">
         <span :id='"prefLabel"+props.index' v-if="props.row.prefLabel">{{props.row.prefLabel}}</span>
-        <span :id='"prefLabel"+props.index' v-if="props.row.name">{{props.row.name}}</span>
         <a class="hover-action fa fa-copy" @click='copyContent("prefLabel"+props.index)'></a>
       </template>
     <template slot="spantext" scope="props">
         <span :id='"spantext"+props.index' v-if="props.row.prefLabel && props.row.notation"></span>
         <a class="hover-action fa fa-copy" title="Notation + Label" @click='copyContentS(props.row.notation + " " + props.row.prefLabel)' v-if="props.row.notation && props.row.prefLabel"></a>
-        <a class="hover-action fa fa-copy" title="Notation + Label" @click='copyContentS(props.row.id + " " + props.row.name)' v-if="props.row.id && props.row.name"></a>
 
         <a class="hover-action fa fa-copy" title="Text span + Notation + Label" @click="doCopy(props.row.notation, props.row.prefLabel)" v-if="props.row.notation && props.row.prefLabel"></a>
-        <a class="hover-action fa fa-copy" title="Text span + Notation + Label" @click="doCopy(props.row.id, props.row.name)" v-if="props.row.id && props.row.name"></a>
       </template>
   </v-server-table>
 </div>
@@ -67,35 +63,6 @@ function copyElementContentS(cps) {
   document.body.removeChild(el);
 }
 
-const keyDict = {
-  0: 'notation',
-  1: 'id',
-  2: 'hp_id',
-  3: ''
-};
-
-const recogDict = {
-  'NCBO Bioportal': 0,
-  'HPO Jax': 1,
-  'Neural Concept Recogniser': 2,
-  'Ontology Lookup Search EBI': 3
-};
-
-function test() {
-  let div = document.getElementsByName("conceptRecogniser")[0];
-  let v = 'NCBO Bioportal';
-  if (typeof div !== "undefined") {
-    v = document.getElementsByClassName('vue-treeselect__single-value')[0].innerText;
-  } 
-  console.log(v + " concept");
-  let x = recogDict[v];
-  // console.log(x);
-  let key = keyDict[x];
-  // console.log(key);
-  console.log('key: ' + key);
-  return key;
-}
-
 export default {
   name: 'search-box',
   components: {
@@ -123,7 +90,7 @@ export default {
           filterPlaceholder: 'Query'
         },
         skin: 'table table-hover',
-        uniqueKey: test()
+        uniqueKey: 'notation'
       },
       query: query,
       ontologyValue: ontology ? [ontology] : [],
@@ -138,12 +105,14 @@ export default {
         id: 'jax',
         label: 'HPO Jax',
       }, {
-        id: 'neural',
-        label: 'Neural Concept Recogniser',
-      }, {
         id: 'ebi',
         label: 'Ontology Lookup Search EBI'
-      }]
+      }, 
+      // {
+      //   id: 'neural',
+      //   label: 'Neural Concept Recogniser',
+      // }
+      ]
     }
   },
   methods: {
@@ -173,9 +142,6 @@ export default {
       window.parent.postMessage({
         type: 'copied'
       }, "*")
-    },
-    getCR() {
-      return 'notation';
     }
   }
 }
--- src/components/SearchBox/table/TableOptions.js
+++ src/components/SearchBox/table/TableOptions.js
@@ -9,44 +9,59 @@ import axios from 'axios';
 
 // JAX = 1 
 //     data : response.data.terms
-//     count : response.data.termsTotalCount
+//     count : response.data.termsTotalCount\
 
-// Neural Concept Recogniser = 2
+// OLS EBI = 2
+//     data : response.data.
+//     count: response.data.
+// https://www.ebi.ac.uk/ols/docs/api/search
+
+// Neural Concept Recogniser = 3
 //     data: response.data.
 //     count: response.data. 
 // https://ncr.ccm.sickkids.ca/api_doc/
 
-// OLS EBI = 3
-//     data : response.data.
-//     count: response.data.
-// https://www.ebi.ac.uk/ols/docs/api
-
 const recogDict = {
   'NCBO Bioportal': 0,
   'HPO Jax': 1,
-  'Neural Concept Recogniser': 2,
-  'Ontology Lookup Search EBI': 3
+  'Ontology Lookup Search EBI': 2,
+  'Neural Concept Recogniser': 3
 };
 
-function test() {
+function getSelectedAPI() {
   let div = document.getElementsByName("conceptRecogniser")[0];
-  let v = 'NCBO Bioportal';
+  let api = 'NCBO Bioportal';
   if (typeof div !== "undefined") {
-    v = document.getElementsByClassName('vue-treeselect__single-value')[0].innerText;
+    api = document.getElementsByClassName('vue-treeselect__single-value')[0].innerText;
   } 
-  // console.log(v + " concept");
-  let x = recogDict[v];
-  // console.log(x);
+  return recogDict[api];
+}
 
-  return x;
+const clone = (obj) => Object.assign({}, obj);
+
+const renameKey = (object, key, newKey) => {
+    const clonedObj = clone(object);
+    const targetKey = clonedObj[key];
+    delete clonedObj[key];
+    clonedObj[newKey] = targetKey;
+    return clonedObj;
+};
+
+function keyLoop(data, init, fin) {
+    var i;
+    var arr = [];
+    for (i = 0; i < data.length; i++) {
+        let temp = renameKey(data[i], init, fin);
+        arr.push(temp);
+    }
+    return arr;
 }
 
 export default {
   requestKeys: {
     query: 'q',
     limit: 'pagesize',
-    page: 'page',
-    byColumn: 'yeet'
+    page: 'page'
   },
   headings: {
     notation: 'Notation',
@@ -57,9 +72,7 @@ export default {
     dropdown: false
   },
   requestFunction: (data) => {
-    let x = test();
-    console.log(x + 'zxczcxz');
-    console.log(data);
+    let api = getSelectedAPI();
     return axios.all([
       axios.get(process.env.API_URL, {
         params: {
@@ -75,35 +88,83 @@ export default {
         params: {
           q: data.q,
         }
-      })
+      }),
+      axios.get('https://www.ebi.ac.uk/ols/api/search', {
+        params: {
+          q: data.q,
+          ontology: 'hp',
+          groupField: 'iri',
+          start: 0,
+          // rows: 5
+        }
+      }),
+      // axios.get('https://ncr.ccm.sickkids.ca/curr/match/', {
+      //   params: {
+      //     text: data.q
+      //   }
+      // })
     ]).then(axios.spread((...responses) => {
       const res0 = responses[0];
       const res1 = responses[1];
       const res2 = responses[2];
       const res3 = responses[3];
 
-      if (x >= 0 && x <= 3) {
-        return responses[x];
+      if (api == 0) {
+        return res0;
+      } else if (api == 1) {
+        res1.data.terms = keyLoop(res1.data.terms, 'id', 'notation');
+        res1.data.terms = keyLoop(res1.data.terms, 'name', 'prefLabel');
+        return res1
+      } else if (api == 2) {
+        res2.data.response.docs = keyLoop(res2.data.response.docs, 'obo_id', 'notation');
+        res2.data.response.docs = keyLoop(res2.data.response.docs, 'label', 'prefLabel');
+        res2.data.response.docs = keyLoop(res2.data.response.docs, 'description', 'definition');
+        return res2;
+      } else if (api == 3) {
+        res3.data.matches = keyLoop(res3.data.matches, 'hp_id', 'notation');
+        var i;
+        for (i = 0; i < res3.data.matches.length; i++) {
+          res3.data.matches[i].prefLabel = res3.data.matches[i].names[0];
+          res3.data.matches[i].names.shift();
+        }
+        res3.data.matches = keyLoop(res3.data.matches, 'names', 'synonym');
+        return res3;
       } else {
-        return null
+        return null;
       }
-    })).catch((e) => {
+    }))
+    .catch((e) => {
       this.dispatch('error', e);
     });
   },
   responseAdapter: (response) => {
-    let x = test();
-    console.log(x + ' asdsad')
+    let api = getSelectedAPI();
+    console.log(response.data);
     if (response.data) {
-      if (x == 0) {
+      if (api == 0) {
         return {
           data: response.data.collection,
           count: response.data.totalCount
         }
-      } else if (x == 1) {
+      } else if (api == 1) {
         return {
           data: response.data.terms,
           count: response.data.termsTotalCount
+        }
+      } else if (api == 2) {
+        return {
+          data: response.data.response.docs,
+          count: response.data.response.numFound
+        }
+      } else if (api == 3) {
+        return {
+          data: response.data.matches,
+          count: response.data.matches.length
+        }
+      } else {
+        return {
+          data: [],
+          count: 0
         }
       }
     } else {
@@ -112,13 +173,6 @@ export default {
         count: 0
       }
     }
-    // return response.data ? {
-    //   data: response.data.collection,
-    //   count: response.data.totalCount
-    // } : {
-    //   data: [],
-    //   count: 0
-    // }
   }
 }
 
